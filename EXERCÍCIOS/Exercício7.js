6e75f3958a379337a9af36f881197f22166c0993
LAURA LETICIA DE OLIVEIRA MEDEIROS <laura.omedeiros198@gmail.com>
2023-03-15 18:44:54 -0300

    Novas atividades concluídas
    completei mais duas atividades do curso de Java Script

---
+++ EXERCÍCIOS/Exercício7.js
@@ -0,0 +1,14 @@
+/*Pratica 4: crie 2 constantes: numero1 e numero2, cada uma contendo um valor numérico. Em seguida, crie mais 5 constantes: soma, subtração, multiplicação, divisão e modulo, que contenha as reespectivas operações usando as constantes numero1 e numero2
+💡 Dicas*/
+
+
+const numero1 = 50;
+const numero2 = 20;
+
+const  soma = numero1 + numero2;
+const subtracao = numero1-numero2;
+const multiplicação = numero1*numero2;
+const divisao = numero1/numero2;
+const modulo= numero1%numero2;
+
+console.log (soma)
---
+++ EXERCÍCIOS/Exercíocio8.js
@@ -0,0 +1,52 @@
+///Estrutura Condiconal
+
+
+/*let hora = 10;
+
+    if (hora < 12){
+        console.log("Bom dia!");
+
+    } else {
+        console.log ("Boa Tarde");
+    }
+
+
+    ///OUTRO EXEMPLO 
+
+
+    let hora = 9
+    if (hora < 5){
+        console.log("voltar a dormi");
+    } else { 
+        console.log ("levantar da cama");
+    }
+
+
+    let hora = 10
+    if (hora > 12){
+        console.log ("Bom dia!");
+    } else if ( hora <10){
+        console.log ("Boa Tarde");
+    } else{
+        console.log (" Boa Noite!");
+    }*/
+
+
+
+/*Crie um arquivo com extensão .js
+Faça:
+Declare um valor numérico;
+Vamos analisar se esse número é ou não maior que 10:
+a) Se for, vamos escrever na tela a seguinte frase: "Esse número é maior que 10";
+b) Se não for, vamos escrever na tela a seguinte frase: "Esse número é menor que 10".*/
+
+let a = 100;
+    b = 100;
+
+if ( a > b){
+    console.log ('a é maior que b ');
+}else if ( a < b ){
+    console.log('a é menor que b ');
+}else{
+    console.log('a é igual a b');
+}
